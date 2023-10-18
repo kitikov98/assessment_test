90bbc3a6c2716ceee7cdf65cbb0e4493b7f515f7
Ilya_Paulovich <ipavlovich007@gmail.com>
2023-10-18 17:10:47 +0300

    добавил меню пользователя, как в оригинале

--- user_panel.py
+++ user_panel.py
@@ -2,10 +2,7 @@ import io
 import telebot
 import json
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
-from database import Database
 from datetime import datetime
-import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from database import Database
 
 with open('keys.json', 'r', encoding='utf-8') as f:  # открыли файл с данными
@@ -16,6 +13,19 @@ tg_group = text['tg_group']
 token = text['user_panel']
 bot = telebot.TeleBot(token)
 
+user_menu = ['Мои аукционы', 'Розыгрыш', 'Топ пользователей', 'Правила', 'Статистика', 'Помощь']
+menu_1 = InlineKeyboardMarkup()
+for item in user_menu:
+    menu_1.add(InlineKeyboardButton(item, callback_data='A'+item))
+
+main_menu = 'Назад в меню'
+menu_2 = InlineKeyboardMarkup()
+menu_2.add(InlineKeyboardButton(main_menu, callback_data='B'+main_menu))
+
+
+@bot.message_handler(commands=['start'])
+def start_admin_panel(message):
+    bot.send_message(message.chat.id, 'Hi', reply_markup=menu_1)
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
@@ -37,6 +47,12 @@ def query_handler(call):
         bot.answer_callback_query(callback_query_id=call.id,
                                   text="""1. Делаем ставку и чёто-там, не забудь поменять""",
                                   show_alert=True)
+
+    elif flag == 'A':
+        bot.edit_message_text("Какой-то текст", call.message.chat.id, call.message.message_id, reply_markup=menu_2)
+    elif flag == 'B':
+        bot.edit_message_text("Какой-то текст", call.message.chat.id, call.message.message_id, reply_markup=menu_1)
+
 
 def user_panel_run():
     print('ready')
