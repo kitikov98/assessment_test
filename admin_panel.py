83c7b3d8ee62cac3d483fb9925ed4471704b2b55
Ilya_Paulovich <ipavlovich007@gmail.com>
2023-10-18 23:55:09 +0300

    сделал загрузку фоток нормально и переделал БД для сохранения отдельно картинок, привязанных к лоту

--- admin_panel.py
+++ admin_panel.py
@@ -18,7 +18,7 @@ token = text['admin_panel']
 bot = telebot.TeleBot(token)
 
 menu_send = InlineKeyboardMarkup()
-menu_send.add(InlineKeyboardButton(text="учавствовать", url="https://t.me/kitikov98_study_bot?start=11"))
+menu_send.add(InlineKeyboardButton(text="учавствовать", url=f"https://t.me/kitikov98_study_bot?start="))
 menu_send.add(InlineKeyboardButton('время', callback_data='t'+'0'), InlineKeyboardButton('info', callback_data='i'+'0'))
 
 menu_1 = ["/Создать_лот", "/Отправка_файла"]
@@ -32,43 +32,45 @@ for x1 in menu_1:
 
 
 list_to_db = []
+photos_list = []
 
 
 def discription_text(list1):
     text = f"""
-Начальная стоимость: {list1[2]}
-id продавца: @{list1[3]}
-местонахождение: {list1[4]}
-описание: {list1[5]}
-время начало торгов: {list1[8]}
-время окончания торгов: {list1[9]}
-тип лота: {list1[10]}
+Начальная стоимость: {list1[1]}
+id продавца: @{list1[2]}
+местонахождение: {list1[3]}
+описание: {list1[4]}
+время начало торгов: {list1[7]}
+время окончания торгов: {list1[8]}
+тип лота: {list1[9]}
 """
     return text
 
 
 @bot.message_handler(commands=['start'])
 def start_admin_panel(message):
-    # Проводим проверку является ли пользователь администратором
     bot.send_message(message.chat.id, 'Hi', reply_markup=admin_markup)
 
 
 @bot.message_handler(commands=['Создать_лот'])
 def start_admin_panel(message):
-    # Проводим проверку является ли пользователь администратором
     msg = bot.send_message(message.chat.id, 'начинаем создавать \nЗагрузите фото')
     bot.register_next_step_handler(msg, photo)
 
 
-@bot.message_handler(content_types=['photo'])
+# @bot.message_handler(content_types=['media_group'])
 def photo(message):
+    # print(message)
     fileID = message.photo[-1].file_id
     file_info = bot.get_file(fileID)
     downloaded_file = bot.download_file(file_info.file_path)
-    list_to_db.append(downloaded_file)
-    msg = bot.send_message(message.chat.id, 'Фото загружено, введите стартовую цену лота')
-    bot.register_next_step_handler(msg, price)
-    print("файл закрыт")
+    photos_list.append(downloaded_file)
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(InlineKeyboardButton('Yes', callback_data='py'), InlineKeyboardButton('No', callback_data='pn'))
+    bot.send_message(message.chat.id, 'Фото загружено, желаете загрузить еще?', reply_markup=keyboard)
+    print("Фото загружено")
+
 
 
 def price(message):
@@ -86,7 +88,7 @@ def geolocation(message):
 
 def description(message):
     list_to_db.append(message.text)
-    msg = bot.send_message(message.chat.id, 'Описание принято, загрузите дополнительную информацию, если есть')
+    msg = bot.send_message(message.chat.id, 'Описание принято, загрузите документ о владении предметом лота')
     bot.register_next_step_handler(msg, document)
 
 
@@ -120,15 +122,32 @@ def finish_time(message):
 
 def lot_type(message):
     list_to_db.append(message.text)
-    # bot.send_message(message.chat.id, 'Тип лота принят, лот выставлен на рассмотрение')
     list_to_db.append('на рассмотрении')
     db.add_lots(list_to_db)
+    lot_id = ''
     with db.connection:
-        lots = db.connection.execute(f'SELECT * FROM lots ORDER BY id DESC LIMIT 1').fetchall()
-        # print(lots)
-    for lot in lots:
-        text_1 = discription_text(lot)
-        bot.send_photo(tg_group, lot[1], caption=text_1, reply_markup=menu_send)
+        lots = db.connection.execute(f'SELECT id FROM lots ORDER BY id DESC ').fetchone() # забираем последнюю запись из бд
+        lot_id = lots[0]       # ид записи
+        for pic in photos_list:
+            db.add_pic(pic, lot_id)
+
+    """ методы для отправки на просмотр"""
+    #         photos = db.connection.execute(f'SELECT pic FROM pic').fetchall()
+    #     # print(lots)
+    # if type(photos) == tuple:
+    #     photos = [photos]
+    # phot_list = []
+    # for phot in photos:
+    #     if photos.index(phot) == 0:
+    #         phot_list.append(telebot.types.InputMediaPhoto(phot[0], caption=''))
+    #     else:
+    #         phot_list.append(telebot.types.InputMediaPhoto(phot[0]))
+    # bot.send_media_group(message.chat.id, phot_list)
+    # for lot in lots:
+    #     text_1 = discription_text(lot)
+    #     bot.send_photo(tg_group, lot[1], caption=text_1, reply_markup=menu_send)
+    list_to_db.clear()
+    photos_list.clear()
 
 
 # @bot.message_handler(commands=['Просмотр_лота'])
@@ -173,6 +192,16 @@ def query_handler(call):
         bot.answer_callback_query(callback_query_id=call.id,
                                   text="""1. Делаем ставку и чёто-там, не забудь поменять""",
                                   show_alert=True)
+    elif flag == 'p':
+        if data == 'y':
+            msg = bot.send_message(call.message.chat.id, 'Загрузите фото')
+            bot.register_next_step_handler(msg, photo)
+        elif data == 'n':
+            msg = bot.send_message(call.message.chat.id, 'Хорошо, введите стартовую цену лота')
+            bot.register_next_step_handler(msg, price)
+
+
+
 
 def admin_panel_run():
     print('ready')
--- database.py
+++ database.py
@@ -18,17 +18,25 @@ class Database:
         self.create_tables()
 
     def create_tables(self):
-        self.connection.execute('''CREATE TABLE IF NOT EXISTS lots (id INTEGER PRIMARY KEY, pic BLOB, 
+        self.connection.execute('''CREATE TABLE IF NOT EXISTS lots (id INTEGER PRIMARY KEY,
         start_price INTEGER, trader_link TEXT, geolocation TEXT, description TEXT, additional_information BLOB,
         file_name TEXT, time_start DATETIME, time_finish DATETIME, type TEXT, status text)''')
+
+        self.connection.execute('''CREATE TABLE IF NOT EXISTS pic (id INTEGER PRIMARY KEY, pic BLOB, lot_id INTEGER)''')
 
         self.connection.commit()
 
     def add_lots(self, list_info):
         with self.connection:
-            self.connection.execute(f"""INSERT INTO lots (pic, start_price, trader_link, geolocation, description,
+            self.connection.execute(f"""INSERT INTO lots (start_price, trader_link, geolocation, description,
              additional_information, file_name, time_start, time_finish, type, status)
-            VALUES(?,?,?,?,?,?,?,?,?,?,?)""", list_info)
+            VALUES(?,?,?,?,?,?,?,?,?,?)""", list_info)
+
+    def add_pic(self, pic, lot_id):
+        with self.connection:
+            self.connection.execute(f"""INSERT INTO pic (pic, lot_id) VALUES(?,?)""", (pic, lot_id))
+
+
 
 
 db = Database('auction.db')
